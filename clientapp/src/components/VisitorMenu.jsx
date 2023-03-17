5c5c59d7e2cad4d9ba3302243584ac4e24ca73bf
Danil <75947542+Dandillo@users.noreply.github.com>
2023-03-17 22:57:28 +0500

    функциональные компоненты

--- clientapp/src/components/VisitorMenu.jsx
+++ clientapp/src/components/VisitorMenu.jsx
@@ -1,49 +1,49 @@
 import React, { Component, useEffect } from "react";
 import Button from "./Button";
 import {Link,Outlet} from 'react-router-dom';
-const VisitorMenu = () => {
+const VisitorMenu = (props) => {
   useEffect(() => {
-    const headerTitle = 'Visitor Menu';
-    this.props.setHeaderTitle(headerTitle);
+    const headerTitle = "Visitor Menu";
+    props.setHeaderTitle(headerTitle);
     document.title = headerTitle;
   });
-    return (
-      <>
-        <div className=" border-nba-border border-2 grid grid-cols-2 w-full items-center justify-between py-[133px] px-[166px] gap-x-[305px] gap-y-[199px]">
+  return (
+    <>
+      <div className=" border-nba-border border-2 grid grid-cols-2 w-full items-center justify-between py-[133px] px-[166px] gap-x-[305px] gap-y-[199px]">
+        <Button
+          color="white"
+          paddings="pt-[32px] pb-[34px]"
+          className="w-[372px]"
+        >
+          Teams
+        </Button>
+        <Button
+          color="white"
+          paddings="pt-[32px] pb-[34px]"
+          className="w-[372px]"
+        >
+          Players
+        </Button>
+        <Button
+          color="white"
+          paddings="pt-[32px] pb-[34px]"
+          className="w-[372px]"
+        >
+          Matchups
+        </Button>
+        <Link to="photos">
           <Button
             color="white"
             paddings="pt-[32px] pb-[34px]"
             className="w-[372px]"
           >
-            Teams
+            Photos
           </Button>
-          <Button
-            color="white"
-            paddings="pt-[32px] pb-[34px]"
-            className="w-[372px]"
-          >
-            Players
-          </Button>
-          <Button
-            color="white"
-            paddings="pt-[32px] pb-[34px]"
-            className="w-[372px]"
-          >
-            Matchups
-          </Button>
-          <Link to="photos">
-            <Button
-              color="white"
-              paddings="pt-[32px] pb-[34px]"
-              className="w-[372px]"
-            >
-              Photos
-            </Button>
-          </Link>
-          <Outlet />
-        </div>
-      </>
-    );
-}
+        </Link>
+        <Outlet />
+      </div>
+    </>
+  );
+};
 
 export default VisitorMenu;
