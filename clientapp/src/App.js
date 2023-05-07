67b00cd009681311f2c731a3f978ee672d4b4765
Ariseko <itamiyoji940@gmail.com>
2023-05-07 14:48:56 +0500

    post request try

--- clientapp/src/App.js
+++ clientapp/src/App.js
@@ -7,6 +7,7 @@ import { Layout } from "./components/Layout";
 import EventAdministratorMenu from "./components/EventAdministratorMenu";
 import AddANewMatchupForRegularSeason from "./components/AddANewMatchupForRegularSeason";
 import TeamsMain from "./components/TeamsMain";
+import AdminLogin from "./components/AdminLogin";
 require("./index.css");
 function App() {
   let location = useLocation();
@@ -14,41 +15,7 @@ function App() {
 
   return (
     <>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Layout
-              headerTitle={headerTitle}
-              header={location.pathname !== "/"}
-            />
-          }
-        >
-          <Route index element={<MainScreen />} />
-          <Route
-            path="visitor"
-            element={<VisitorMenu setHeaderTitle={setHeaderTitle} />}
-          />
-          <Route
-            path="visitor/photos"
-            element={<Photos setHeaderTitle={setHeaderTitle} />}
-          />
-          <Route
-            path="eventMenu"
-            element={<EventAdministratorMenu setHeaderTitle={setHeaderTitle} />}
-          />
-          <Route
-            path="matchups/create-new"
-            element={
-              <AddANewMatchupForRegularSeason setHeaderTitle={setHeaderTitle} />
-            }
-          />
-          <Route
-            path="visitor/teams"
-            element={<TeamsMain setHeaderTitle={setHeaderTitle} />}
-          />
-        </Route>
-      </Routes>
+      <AdminLogin/>
     </>
   );
 }
--- clientapp/src/components/AdminLogin.jsx
+++ clientapp/src/components/AdminLogin.jsx
@@ -1,47 +1,66 @@
-import React from "react";
+import React, {useState} from "react";
 import {Header} from "./Header";
 import Footer from "./Footer";
 import {Button, Typography} from "@mui/material";
 
 
 const AdminLogin = () => {
+  const [jobNumber, password] = useState("");
+
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    const data = { jobNumber, password };
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    };
+
+
+    fetch("http://176.124.192.232/api/Authorization/login", requestOptions)
+  };
+
   return(
+
     <>
       <Header/>
       <div className="app">
 
         <p className="text-center text-nba-textGray text-[25px] my-10">Users can login into the system using their jobnumber and password.</p>
 
+        <form action="">
+
         <div className="text-center flex flex-col justify-between h-full">
           <div>
             <Typography className="text-nba-textGray text-[25px] my-10">
-              Jobnumber: <input className='border-solid border-2 ml-[20px] text-[28px] px-[14px]' type="text"/>
+              Jobnumber: <input value={jobNumber} className='border-solid border-2 ml-[20px] text-[28px] px-[14px]' type="text"/>
             </Typography>
           </div>
           <div>
             <Typography className="text-nba-textGray text-[25px] my-10">
-              Password:  <input className='border-solid border-2 ml-[20px] text-[28px] px-[14px] px-1.5' type="password"/>
+              Password:  <input value={password} className='border-solid border-2 ml-[20px] text-[28px] px-[14px] px-1.5' type="password"/>
             </Typography>
           </div>
           <p><input className="my-10" type="checkbox" name="remember" /> Remember me </p>
 
           <div className="my-10">
-            <Button
-              variant="standard"
-              className="w-[40px]  bg-nba-wheat  text-[10px] nba-textGray mx-10">
-              Login
-            </Button>
+              <Button
+                variant="standard"
+                className="w-[40px]  bg-nba-wheat  text-[10px] nba-textGray mx-10"
+                onClick={ handleSubmit }>
+                Login
+              </Button>
 
-            <Button
-              variant="standard"
-              className="w-[40px]  bg-nba-wheat  text-[10px] nba-textGray mx-10">
-              Cancel
-            </Button>
-
+              <Button
+                variant="standard"
+                className="w-[40px]  bg-nba-wheat  text-[10px] nba-textGray mx-10">
+                Cancel
+              </Button>
           </div>
-
         </div>
-
+      </form>
 
       </div>
 
